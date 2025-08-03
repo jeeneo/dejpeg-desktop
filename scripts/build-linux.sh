7626d2d97e16b565ef9bdccefd0df6ba1e13ba02
jeeneo <187322222+jeeneo@users.noreply.github.com>
2025-08-03 17:11:53 -0400

    fix building, move button

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -9,28 +9,28 @@ if [ -d node_modules ]; then
   exit 1
 fi
 
+rm -rf dist
+
 mv node_modules_linux node_modules
 
-tsc && cp -r src/static dist
-pkg . --target node18-linux-x64 --output linux-build/dejpeg-linux
+tsc
+cp -r src/static dist
+pkg . --target node18-linux-x64 --output builds/linux-build/dejpeg-linux
 
 mv node_modules node_modules_linux
 
-mkdir -p linux-build/node_modules
-cp -r node_modules_linux/onnxruntime-node linux-build/node_modules
-cp -r node_modules_linux/sharp linux-build/node_modules
+mkdir -p builds/linux-build/node_modules
+cp -r node_modules_linux/onnxruntime-node builds/linux-build/node_modules
+cp -r node_modules_linux/sharp builds/linux-build/node_modules
 
-# removing unnecessary binaries
-rm -rf linux-build/node_modules/onnxruntime-node/bin/napi-v6/darwin
-rm -rf linux-build/node_modules/onnxruntime-node/bin/napi-v6/win32
+rm -rf builds/linux-build/node_modules/onnxruntime-node/bin/napi-v6/darwin
+rm -rf builds/linux-build/node_modules/onnxruntime-node/bin/napi-v6/win32
 
-# stripping unnecessary files
-rm -rf linux-build/node_modules/onnxruntime-node/bin/napi-v6/linux/arm64
-rm linux-build/node_modules/onnxruntime-node/bin/napi-v6/linux/x64/libonnxruntime_providers_cuda.so
-rm linux-build/node_modules/onnxruntime-node/bin/napi-v6/linux/x64/*.bak
+rm -rf builds/linux-build/node_modules/onnxruntime-node/bin/napi-v6/linux/arm64
+rm builds/linux-build/node_modules/onnxruntime-node/bin/napi-v6/linux/x64/libonnxruntime_providers_cuda.so
+rm builds/linux-build/node_modules/onnxruntime-node/bin/napi-v6/linux/x64/*.bak
 
-7z a -t7z -mx=9 ./dejpeg-linux.7z ./linux-build
-# clean build directory
-rm -rf linux-build
+7z a -t7z -mx=9 builds/dejpeg-linux.7z ./builds/linux-build
+rm -rf builds/linux-build
 
 echo "build complete: dejpeg-linux"
--- scripts/build-win.sh
+++ scripts/build-win.sh
@@ -9,25 +9,26 @@ if [ -d node_modules ]; then
   exit 1
 fi
 
+rm -rf dist
+
 mv node_modules_windows node_modules
 
-tsc && cp -r src/static dist
-mkdir -p win-build
-pkg . --target node18-win-x64 --output win-build/dejpeg-win.exe
+tsc
+cp -r src/static dist
+pkg . --target node18-win-x64 --output builds/win-build/dejpeg-win.exe
 
 mv node_modules node_modules_windows
 
-mkdir -p win-build/node_modules
-cp -r node_modules_windows/onnxruntime-node win-build/node_modules
-cp -r node_modules_windows/sharp win-build/node_modules
+mkdir -p builds/win-build/node_modules
+cp -r node_modules_windows/onnxruntime-node builds/win-build/node_modules
+cp -r node_modules_windows/sharp builds/win-build/node_modules
 
-rm -rf win-build/node_modules/onnxruntime-node/bin/napi-v6/darwin
-rm -rf win-build/node_modules/onnxruntime-node/bin/napi-v6/linux
+rm -rf builds/win-build/node_modules/onnxruntime-node/bin/napi-v6/darwin
+rm -rf builds/win-build/node_modules/onnxruntime-node/bin/napi-v6/linux
 
-rm -rf win-build/node_modules/onnxruntime-node/bin/napi-v6/win32/arm64
+rm -rf builds/win-build/node_modules/onnxruntime-node/bin/napi-v6/win32/arm64
 
-7z a -t7z -mx=9 ./dejpeg-win.7z ./win-build/*
-# clean build directory
-rm -rf win-build
+7z a -t7z -mx=9 builds/dejpeg-win.7z ./builds/win-build
+rm -rf builds/win-build
 
 echo "build complete: dejpeg-win.7z"
